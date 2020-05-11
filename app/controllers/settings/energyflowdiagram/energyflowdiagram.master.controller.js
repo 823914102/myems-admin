2c61e77e21b238c59e0a57ba309a3ac799d46210
guangyuzhang <13621160019@163.com>
2020-05-11 11:38:50 +0800

    added broadcast handlers to enregy flow diagram controllers

--- app/controllers/settings/energyflowdiagram/energyflowdiagram.master.controller.js
+++ app/controllers/settings/energyflowdiagram/energyflowdiagram.master.controller.js
@@ -2,8 +2,16 @@
 
 app.controller('EnergyFlowDiagramMasterController', function($scope) {
 
-	$scope.$on('handleEmitEnergyFlowDiagramChanged', function(event) {
-		$scope.$broadcast('handleBroadcastEnergyFlowDiagramChanged');
-	});
+		$scope.$on('handleEmitEnergyFlowDiagramChanged', function(event) {
+			$scope.$broadcast('handleBroadcastEnergyFlowDiagramChanged');
+		});
+
+		$scope.$on('handleEmitEnergyFlowDiagramNodeChanged', function(event) {
+			$scope.$broadcast('handleBroadcastEnergyFlowDiagramNodeChanged');
+		});
+
+		$scope.$on('handleEmitEnergyFlowDiagramLinkChanged', function(event) {
+			$scope.$broadcast('handleBroadcastEnergyFlowDiagramLinkChanged');
+		});
 
 });
