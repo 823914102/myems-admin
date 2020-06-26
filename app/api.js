a55c7fee63e00759814111a0f1722a6c5720f6c6
guangyuzhang <13621160019@163.com>
2020-06-26 11:24:15 +0800

    updated base url in api.js

--- app/api.js
+++ app/api.js
@@ -1,5 +1,5 @@
 //Switch Classes Function
 function getAPI(){
-    return "http://10.111.212.140:8080/";
+    return "http://127.0.0.1:8080/";
     //return "http://"+window.location.hostname+":8080/";
 }
