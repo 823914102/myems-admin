b40f17e9a924563f2be49ff241113070d60829ff
guangyuzhang <13621160019@163.com>
2020-08-05 13:44:16 +0800

    impelmented Shopfloor

--- app/controllers/settings/space/space.master.controller.js
+++ app/controllers/settings/space/space.master.controller.js
@@ -5,10 +5,4 @@ app.controller('SpaceMasterController', function($scope) {
 	$scope.$on('handleEmitSpaceChanged', function(event) {
 		$scope.$broadcast('handleBroadcastSpaceChanged');
 	});
-
-	$scope.$on('handleEmitTenantChanged', function(event) {
-		$scope.$broadcast('handleBroadcastTenantChanged');
-	});
-
-
 });
