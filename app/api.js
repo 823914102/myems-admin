398c7795e70503a0a33373a68995db579162f816
guangyuzhang <13621160019@163.com>
2020-09-27 17:39:09 +0800

    added comment to app/api.js

--- app/api.js
+++ app/api.js
@@ -2,5 +2,6 @@
 function getAPI(){
     //return "http://127.0.0.1:8080/";
     //return "http://"+window.location.hostname+":8080/api/";
+    // to avoid CORS issue, use Nginx to proxy myems-api to path /api with the same ip and port as myems-web
     return "http://"+window.location.hostname+"/api/";
 }
