3a4606be74dc373209b5f3db26a501b6f4525dc3
guangyuzhang <13621160019@163.com>
2020-09-27 18:39:12 +0800

    updated api url in app/api.js

--- app/api.js
+++ app/api.js
@@ -1,7 +1,6 @@
 //Switch Classes Function
 function getAPI(){
     //return "http://127.0.0.1:8080/";
-    //return "http://"+window.location.hostname+":8080/api/";
     // to avoid CORS issue, use Nginx to proxy myems-api to path /api with the same ip and port as myems-web
-    return "http://"+window.location.hostname+"/api/";
+    return window.location.protocol+"//"+window.location.hostname+":"+window.location.port+"/api/";
 }
