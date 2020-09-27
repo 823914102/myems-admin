76cb6c862b8c300e3d3db24e656432629dd96481
guangyuzhang <13621160019@163.com>
2020-09-27 17:34:40 +0800

    updated api.js

--- app/api.js
+++ app/api.js
@@ -1,5 +1,6 @@
 //Switch Classes Function
 function getAPI(){
     //return "http://127.0.0.1:8080/";
-    return "http://"+window.location.hostname+":/api/";
+    //return "http://"+window.location.hostname+":8080/api/";
+    return "http://"+window.location.hostname+"/api/";
 }
